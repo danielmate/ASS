5067034e09aa6407ae5fecc331400e07e3f8de25
danielmate <danyel.mate@gmail.com>
2017-03-19 21:44:15 +0200

    Commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Parcare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Parcare
+    {
+        public string nrloc;
+        public string timp;
+        public string numeparcare;
+        public string numeconducator;
+        public string numarinmatriculare;
+        public Parcare(string loc, string t, string np, string nc, string ni)
+        {
+            nrloc = loc;
+            timp = t;
+            numeparcare = np;
+            numeconducator = nc;
+            numarinmatriculare = ni;
+        }
+    }
+}
